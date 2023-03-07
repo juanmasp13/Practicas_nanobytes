bcf5e2b0a2c7636ed2b5ad7e3d631ce4e1721bc7
juanmasp13 <127190835+juanmasp13@users.noreply.github.com>
2023-03-07 13:48:34 +0100

    Add files via upload

---
+++ gap_LANDSCAPE_TERMINOS/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'GAP V1 LANDSCAPE CONDICIONES LEGALES',
+    'sequence': -1005,
+    'summary': 'Añadir botón para los términos legales',
+    'description': """Botón para los términos legales de la compra""",
+    'depends': ['website_sale','web','base','website'],
+    'data': ['views/inherit_web_purchase.xml'],
+    'auto_install': False,
+    'assets': {
+        'web.assets_frontend': [
+            'gap_LANDSCAPE_TERMINOS/static/src/js/boton_terminos.js',
+        ],
+    },
+    'application': True
+}
---
+++ gap_LANDSCAPE_TERMINOS/static/src/js/boton_terminos.js
@@ -0,0 +1,29 @@
+odoo.define('gap_LANDSCAPE_TERMINOS.javascript', function(require) {
+    'use strict'
+    console.log('holaaaaaaaa');
+
+
+    var core = require('web.core');
+    var config = require('web.config');
+    var publicWidget = require('web.public.widget');
+    var VariantMixin = require('website_sale.VariantMixin');
+/*
+   publicWidget.registry.WebsiteSale = publicWidget.Widget.extend(VariantMixin, {
+    selector: '.oe_website_sale',
+    events: _.extend({}, VariantMixin.events || {}, {
+        'change #checkbox_terminos': '_onChangeNext',
+    }),
+
+    _onChangeNext: function () {
+        if ($("#checkbox_terminos").checked){
+            $('.a-submit').addClass('disabled');
+            console.log('CHECKEADO');
+        }else{
+            $('.a-submit').removeClass('disabled');
+            console.log('NO CHECKEADO');
+        }
+
+    }
+});
+*/
+});
