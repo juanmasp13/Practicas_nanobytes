3c4da7ec38540f2bf405b953f232f6dc92434150
Juan Manuel Santiago Puentes <juanmasp888@gmail.com>
2023-03-08 12:54:24 +0100

    Sí me coge la etiqueta, ahora procedo a agregarle la clase 'disabled'

--- gap_LANDSCAPE_TERMINOS/static/src/js/boton_terminos.js
+++ gap_LANDSCAPE_TERMINOS/static/src/js/boton_terminos.js
@@ -18,9 +18,11 @@ odoo.define('gap_LANDSCAPE_TERMINOS.javascript', function(require) {
                 if(self.$el.find('#checkbox_terminos').is(':checked')){
                     console.log('marcado');
                     console.log('MI BOTON: '+boton);
+                    boton.addClass('disabled');
                 }else{
                     console.log('desmarcado');
                     console.log('MI BOTON: '+boton);
+                    boton.removeClass('disabled');
                 }
 
             });
