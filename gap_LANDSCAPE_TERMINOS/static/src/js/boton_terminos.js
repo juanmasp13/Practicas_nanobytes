d03040f38eabab548829cfa0c343323459566595
Juan Manuel Santiago Puentes <juanmasp888@gmail.com>
2023-03-08 12:23:05 +0100

    he borrado la función value

--- gap_LANDSCAPE_TERMINOS/static/src/js/boton_terminos.js
+++ gap_LANDSCAPE_TERMINOS/static/src/js/boton_terminos.js
@@ -13,7 +13,7 @@ odoo.define('gap_LANDSCAPE_TERMINOS.javascript', function(require) {
             var self = this;
             this.$el.find('#checkbox_terminos').on('change', function (ev) {
 
-                console.log('ESTADO CHECKBOX: '+self.$el.find('#checkbox_terminos').value)
+                console.log('ESTADO CHECKBOX: '+self.$el.find('#checkbox_terminos'))
 
                 if(self.$el.find('#checkbox_terminos').checked){
                     console.log('marcado');
