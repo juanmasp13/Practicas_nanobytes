5cf5b8e36289656d0916621932355dfe18581405
Juan Manuel Santiago Puentes <juanmasp888@gmail.com>
2023-03-09 12:42:48 +0100

    He creado una carpeta wizard, también su modelo correspondiente, con un campo many2one y otro tipo binary, también he creado un archivo de seguridad para este nuevo modelo, y todo está importado en el init y el manifest

--- gap_importador/__manifest__.py
+++ gap_importador/__manifest__.py
@@ -4,7 +4,9 @@
     'summary': 'GAP V2 Viator Plus Ultra Importador de productos y tarifas',
     'description': """GAP V2 Viator Plus Ultra Importador de productos y tarifas""",
     'depends': ['stock'],
-    'data': ['views/menu_gap.xml'],
+    'data': [
+            'views/menu_gap.xml',
+            'security/ir.model.access.csv',],
     'auto_install': False,
     'application': True
 }
---
+++ gap_importador/wizard/import_wizard.py
@@ -0,0 +1,8 @@
+from odoo import models, fields, api
+
+class importProductsWizard(models.TransientModel):
+    _name = 'import.products.wizard'
+    _description = 'Import Products Wizard'
+
+    category_id = fields.Many2one('product.category', string="Categoría")
+    fichero = fields.Binary(string="Documento")
