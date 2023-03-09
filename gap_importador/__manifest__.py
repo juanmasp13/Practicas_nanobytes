afd5d108099ab954ed26aa1a7086e8503bc80c9d
Juan Manuel Santiago Puentes <juanmasp888@gmail.com>
2023-03-09 13:17:31 +0100

    He creado la acción correspondiente para abrir una vista de mi wizard, además he tenido que crear esta vista de tipo form, también he cambiado el orden de la declaración de los archivos del manifest de forma que el archivo de seguridad esté primero

--- gap_importador/__manifest__.py
+++ gap_importador/__manifest__.py
@@ -5,8 +5,9 @@
     'description': """GAP V2 Viator Plus Ultra Importador de productos y tarifas""",
     'depends': ['stock'],
     'data': [
+            'security/ir.model.access.csv',
             'wizard/import_products_view.xml',
-            'security/ir.model.access.csv',],
+            ],
     'auto_install': False,
     'application': True
 }
