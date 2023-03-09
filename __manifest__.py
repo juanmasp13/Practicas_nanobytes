8d389753a07b55fa9eb4ec966e9650d40c4aaa85
Juan Manuel Santiago Puentes <juanmasp888@gmail.com>
2023-03-09 11:11:45 +0100

    añadiendo el init y el manifest

---
+++ __manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'GAP V2 Viator Plus Ultra Importador de productos y tarifas',
+    'sequence': -1006,
+    'summary': 'GAP V2 Viator Plus Ultra Importador de productos y tarifas',
+    'description': """GAP V2 Viator Plus Ultra Importador de productos y tarifas""",
+    'depends': ['inventory'],
+    'data': [],
+    'auto_install': False,
+    'application': True
+}
