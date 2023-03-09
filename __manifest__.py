aca6c939f3f8b3686df38edf9faf46645f9bd534
Juan Manuel Santiago Puentes <juanmasp888@gmail.com>
2023-03-09 11:16:09 +0100

    He añadido un menuitem

--- __manifest__.py
+++ __manifest__.py
@@ -4,7 +4,7 @@
     'summary': 'GAP V2 Viator Plus Ultra Importador de productos y tarifas',
     'description': """GAP V2 Viator Plus Ultra Importador de productos y tarifas""",
     'depends': ['inventory'],
-    'data': [],
+    'data': ['views/menugap_xml'],
     'auto_install': False,
     'application': True
 }
