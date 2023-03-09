bc0dc87f9d2c7aeceb4fd879cdd1fd55ef748032
Juan Manuel Santiago Puentes <juanmasp888@gmail.com>
2023-03-09 09:49:36 +0100

    He creado la clase disabled en css, y he añadido los métodos addClass y removeClass en sus respectivas condiciones, además he hecho que al iniciar la página el botón automáticamente este con la clase disabled

--- gap_LANDSCAPE_TERMINOS/static/src/js/boton_terminos.js
+++ gap_LANDSCAPE_TERMINOS/static/src/js/boton_terminos.js
@@ -11,20 +11,21 @@ odoo.define('gap_LANDSCAPE_TERMINOS.javascript', function(require) {
         start: function () {
             console.log('entro');
             var self = this;
-            this.$el.find('#checkbox_terminos').on('change', function (ev) {
-                var boton = self.$el.find('.btn.btn-primary.mb32.a-submit.a-submit-disable.a-submit-loading');
+            var boton = self.$el.find('.btn.btn-primary.mb32.a-submit.a-submit-disable.a-submit-loading');
+            boton.addClass('disabled');
+            self.$el.find('#checkbox_terminos').on('change', function (ev) {
                 if(self.$el.find('#checkbox_terminos').is(':checked')){
                     console.log('marcado');
                     console.log('MI BOTON: '+boton);
                     console.log(boton);
-                    //boton.addClass('juanma');
-                    boton.hide();
+                    boton.removeClass('disabled');
+                    //boton.hide();
                 }else{
                     console.log('desmarcado');
                     console.log('MI BOTON: '+boton);
                     console.log(boton);
-                    //boton.removeClass('juanma');
-                    boton.show();
+                    boton.addClass('disabled');
+                    //boton.show();
                 }
 
             });
