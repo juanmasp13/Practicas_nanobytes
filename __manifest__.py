a42d479d7f3badddef1d57af9b25ca893ee17882
Juan Manuel Santiago Puentes <juanmasp888@gmail.com>
2023-03-09 11:16:56 +0100

    fix manifest

--- __manifest__.py
+++ __manifest__.py
@@ -3,8 +3,8 @@
     'sequence': -1006,
     'summary': 'GAP V2 Viator Plus Ultra Importador de productos y tarifas',
     'description': """GAP V2 Viator Plus Ultra Importador de productos y tarifas""",
-    'depends': ['inventory'],
-    'data': ['views/menugap_xml'],
+    'depends': ['stock'],
+    'data': ['views/menu_gap.xml'],
     'auto_install': False,
     'application': True
 }
