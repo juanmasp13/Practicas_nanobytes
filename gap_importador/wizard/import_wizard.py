d31961c0f3b53b90774d0a46a8a5d53cb9a12427
Juan Manuel Santiago Puentes <juanmasp888@gmail.com>
2023-03-13 11:59:19 +0100

    He agregado otro método que hacía falta

--- gap_importador/wizard/import_wizard.py
+++ gap_importador/wizard/import_wizard.py
@@ -21,7 +21,7 @@ class importProductsWizard(models.TransientModel):
         for record in self:
             if record.fichero:
                 logger.info('FICHERO BINARIO DESPUES DE USAR IOBYTES')
-                doc = ODSReader(file=io.BytesIO(self.fichero or b''))
+                doc = ODSReader(file=io.BytesIO(record.fichero or b''))
                 contenido = doc._read_ods
                 logger.info(contenido)
                 # logger.info(doc._read_ods)
