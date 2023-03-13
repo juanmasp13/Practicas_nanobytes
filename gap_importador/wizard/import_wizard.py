a68415461f1c3655edc7d0499c8f8f908b0929d1
Juan Manuel Santiago Puentes <juanmasp888@gmail.com>
2023-03-13 10:26:15 +0100

    Ya no creo archivos temporales, si no, que me genero un objeto de tipo binario gracias a io.BytesIO(fields.binary) y así poder recorrer los datos de este objeto

--- gap_importador/wizard/import_wizard.py
+++ gap_importador/wizard/import_wizard.py
@@ -1,7 +1,6 @@
 from odoo import models, fields, api
 from openpyxl import load_workbook as lw
-import logging, os
-import tempfile
+import logging, io
 logger = logging.getLogger(__name__)
 
 class importProductsWizard(models.TransientModel):
@@ -17,36 +16,29 @@ class importProductsWizard(models.TransientModel):
         logger.info(self.fichero)
         for record in self:
             if record.fichero:
-                # Crea un archivo temporal
+                
+                # Crea un objeto de bytes con el contenido del archivo cargado
+                mi_fichero = io.BytesIO(record.fichero)
 
-                with tempfile.NamedTemporaryFile(dir='.', suffix='.xlsx') as tmp_file:
-                    # Copia los datos del archivo cargado al archivo temporal
-                    tmp_file.write(record.fichero)
-                    tmp_file.flush()
-                    # Obtiene la ruta del archivo temporal
-                    file_path = tmp_file.name
-                    logger.info('MI RUTA:')
-                    logger.info(file_path)
+                excel = lw(mi_fichero)
+                logger.info('leo el archivo')
+                hojas = excel.active
 
-                    excel = lw(file_path)
-                    logger.info('leo el archivo')
-                    hojas = excel.active
+                # filas = hojas.rows
+                # next(filas)
 
-                    # filas = hojas.rows
-                    # next(filas)
+                filas_totales = []
 
-                    filas_totales = []
+                for fila in hojas.iter_rows(values_only=True):
+                    filas_totales.append(fila)
+                # for fila in filas:
+                #     datos = {'name': '', 'detailed_type': ''}
+                #     for titulo, celda in zip(datos.keys(), fila):
+                #         datos[titulo] = celda.value
+                    
+                #     filas_totales.append(datos)
 
-                    for fila in hojas.iter_rows(values_only=True):
-                        filas_totales.append(fila)
-                    # for fila in filas:
-                    #     datos = {'name': '', 'detailed_type': ''}
-                    #     for titulo, celda in zip(datos.keys(), fila):
-                    #         datos[titulo] = celda.value
-                        
-                    #     filas_totales.append(datos)
-
-                    logger.info(filas_totales)
+                logger.info(filas_totales)
         
 
     # @api.multi
