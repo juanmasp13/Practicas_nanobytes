4cca93b12aa98762140d3c2cb8d8494ce09efb52
Juan Manuel Santiago Puentes <juanmasp888@gmail.com>
2023-03-16 12:50:34 +0100

    MENSAJES DE DEPURACION

--- gap_importador/wizard/import_wizard.py
+++ gap_importador/wizard/import_wizard.py
@@ -111,12 +111,15 @@ class importProductsWizard(models.TransientModel):
                                         logger.info('SI EXISTE EL TEMPLATE %s' % template.name)
                                         attribute_line = self.env['product.template.attribute.line'].search([('attribute_id', '=', atributo.id)])
                                         if attribute_line:
+                                            logger.info('SI EXISTE EL ATTRIBUTE LINE PARA EL ATRIBUTO %s' % atributo.name)
                                             attribute_line_vals_ids = attribute_line.value_ids.ids
                                             lista_id = list(dict.fromkeys(lista_id+attribute_line_vals_ids))
                                             logger.info('REESCRIBIENDO ATTRIBUTE LINE CON LOS VALORES DE LOS ATRIBUTOS %s' % lista_id)
                                             attribute_line.write({'value_ids': [(6, 0, lista_id)]})
                                         else:
+                                            logger.info('NO EXISTE EL ATTRIBUTE LINE PARA EL ATRIBUTO %s' % atributo.name)
                                             self.env['product.template.attribute.line'].create({'attribute_id': atributo.id, 'product_tmpl_id': template.id, 'value_ids': lista_id})
+                                            logger.info('ATTRIBUTE LINE CREADA')
                                     else:
                                         logger.info('NO EXISTE EL TEMPLATE %s' % fila[3])
                                         producto = self.env['product.template'].create({'name': fila[3], 'categ_id': record.category_id.id, 'detailed_type': 'product'})
