76d10faf4b1b252cfd81cb1d50675abbe8ad8f7e
Juan Manuel Santiago Puentes <juanmasp888@gmail.com>
2023-03-17 14:22:39 +0100

    Ya tengo los product.product, ahora voy a comprobar que me introduce los valores correctamente

--- gap_importador/wizard/import_wizard.py
+++ gap_importador/wizard/import_wizard.py
@@ -124,7 +124,8 @@ class importProductsWizard(models.TransientModel):
                 combinacion = self.concatenar_combinacion(ptav1, ptav2)
                 id_producto = self.env['product.product'].search([('combination_indices', '=', combinacion)]).id
                 producto = self.env['product.product'].browse([id_producto])
-                producto.write({'barcode': fila[1], 'default_code': fila[2], 'description': fila[11], 'standard_price': fila[12]})
+                divisa = self.env['res.currency'].search([('name', '=', fila[13])]).id
+                producto.write({'barcode': fila[1], 'default_code': fila[2], 'description': fila[11], 'standard_price': fila[12], 'currency_id': divisa})
 
                 
                 
