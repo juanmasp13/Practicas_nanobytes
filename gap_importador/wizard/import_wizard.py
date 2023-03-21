e035bf9c3bc619c0dd778c295e8777183d5b8a6a
Juan Manuel Santiago Puentes <juanmasp888@gmail.com>
2023-03-21 12:21:48 +0100

    Creando los pricelist

--- gap_importador/wizard/import_wizard.py
+++ gap_importador/wizard/import_wizard.py
@@ -142,7 +142,6 @@ class importProductsWizard(models.TransientModel):
                 producto = self.env['product.product'].search([('combination_indices', '=', combinacion)])
                 logger.info('PARA EL PRODUCTO %s' % producto.display_name)
                 divisa = self.env['res.currency'].search([('name', '=', fila[13])]).id
-                #PVP 1: 14, PVP 2: 15, PVP 3: 16, PVP Final: 17
                 if fila[0] != '':
                     external_id = self.env['ir.model.data'].search([('name', '=', fila[0])])               
                     if external_id:
@@ -154,6 +153,27 @@ class importProductsWizard(models.TransientModel):
                     pricelist_item = self.env['product.pricelist.item'].search([('pricelist_id', '=', pricelist_id), ('product_id', '=', producto.id)])
                     if pricelist_item:
                         pricelist_item.write({'fixed_price': fila[14]})
+                    else:
+                        pricelist_item = self.env['product.pricelist.item'].create({'pricelist_id': pricelist_id, 'product_id': producto.id, 'fixed_price': fila[14]})
+                if fila[15] != '':
+                    pricelist_id = self.env['product.pricelist'].search([('name', '=', 'PVP 2')]).id
+                    pricelist_item = self.env['product.pricelist.item'].search([('pricelist_id', '=', pricelist_id), ('product_id', '=', producto.id)])
+                    if pricelist_item:
+                        pricelist_item.write({'fixed_price': fila[15]})
+                    else:
+                        pricelist_item = self.env['product.pricelist.item'].create({'pricelist_id': pricelist_id, 'product_id': producto.id, 'fixed_price': fila[14]})
+                if fila[16] != '':
+                    pricelist_id = self.env['product.pricelist'].search([('name', '=', 'PVP 3')]).id
+                    pricelist_item = self.env['product.pricelist.item'].search([('pricelist_id', '=', pricelist_id), ('product_id', '=', producto.id)])
+                    if pricelist_item:
+                        pricelist_item.write({'fixed_price': fila[16]})
+                    else:
+                        pricelist_item = self.env['product.pricelist.item'].create({'pricelist_id': pricelist_id, 'product_id': producto.id, 'fixed_price': fila[14]})
+                if fila[17] != '':
+                    pricelist_id = self.env['product.pricelist'].search([('name', '=', 'PVP Final')]).id
+                    pricelist_item = self.env['product.pricelist.item'].search([('pricelist_id', '=', pricelist_id), ('product_id', '=', producto.id)])
+                    if pricelist_item:
+                        pricelist_item.write({'fixed_price': fila[17]})
                     else:
                         pricelist_item = self.env['product.pricelist.item'].create({'pricelist_id': pricelist_id, 'product_id': producto.id, 'fixed_price': fila[14]})
                 producto.write({'barcode': fila[1], 'default_code': fila[2], 'description': fila[11], 'standard_price': fila[12], 'currency_id': divisa})
