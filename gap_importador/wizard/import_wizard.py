5a7c04c7d481a0494894a7a35d8d5abf76c3d577
Juan Manuel Santiago Puentes <juanmasp888@gmail.com>
2023-03-22 09:23:17 +0100

    Arreglar el error que dice que no puedo concatenar un booleano con un string

--- gap_importador/wizard/import_wizard.py
+++ gap_importador/wizard/import_wizard.py
@@ -97,7 +97,7 @@ class importProductsWizard(models.TransientModel):
                         lista_id2.append(valores_attr2.id)
                     else:
                         log = "Para el atributo %s no existe el valor %s" % (atributo2.name, fila[9])
-                                              
+                        logger.info("EL CAMPO LOG INFORMACIÓN: %s" % self.log_importacion)                      
                     if valores_attr1 and valores_attr2: #Si existe el template buscamos si existe algun attribute line
                         if template:
                             logger.info("EXISTE EL TEMPLATE %s" % template.name)
@@ -122,10 +122,10 @@ class importProductsWizard(models.TransientModel):
                             attribute_line2 = self.env['product.template.attribute.line'].create({'attribute_id': atributo2.id, 'product_tmpl_id': product_template.id, 'value_ids': lista_id2})
                 else:
                     log = "El atributo %s no existe" % fila[6]
-                    
+                    logger.info("EL CAMPO LOG INFORMACIÓN: %s" % self.log_importacion)
             else:
                 log = "El atributo %s no existe" % fila[5]
-                 
+                logger.info("EL CAMPO LOG INFORMACIÓN: %s" % self.log_importacion) 
 
                 
         return filas
