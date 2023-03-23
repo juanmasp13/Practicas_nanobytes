bf0904576c97765c76dba3d3aecec7798e49cebd
Juan Manuel Santiago Puentes <juanmasp888@gmail.com>
2023-03-23 13:22:08 +0100

    He añadido el init y el manifest

---
+++ gap_paragon/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'GAP V1 PARAGON REQUERIMIENTOS DE VENTAS E INVENTARIO',
+    'sequence': -1001,
+    'summary': 'Gap de paragon',
+    'description': """Gap de paragon""",
+    'depends': ['base_setup','stock','stock_barcode'],
+    'data': [],
+    'auto_install': False,
+    'application': True
+}
