db8c54ac57685a9661ee6940b9bf303545d434a4
Juan Manuel Santiago Puentes <juanmasp888@gmail.com>
2023-03-23 10:04:48 +0100

    He cambiado el operador = por in, en los search que le paso una lista de ids

--- gap_importador/wizard/import_wizard.py
+++ gap_importador/wizard/import_wizard.py
@@ -113,9 +113,9 @@ class importProductsWizard(models.TransientModel):
                     {'attribute_id': atributo2.id, 'product_tmpl_id': template.id, 'value_ids': [valores_attr2.id,]}]
                     ).ids
 
-            ids_combinacion = self.env['product.template.attribute.value'].search([('product_tmpl_id', '=', template.id), ('attribute_line_id', '=', attribute_lines)]).ids
+            ids_combinacion = self.env['product.template.attribute.value'].search([('product_tmpl_id', '=', template.id), ('attribute_line_id', 'in', attribute_lines)]).ids
             sorted(ids_combinacion)
-            ids_no_descatalogados.append(self.env['product.product'].search([('combination_indices', '=', ids_combinacion)]).id)
+            ids_no_descatalogados.append(self.env['product.product'].search([('combination_indices', 'in', ids_combinacion)]).id)
             num_fila += 1        
 
         if self.descatalogar: #Si la casilla de descatalogar está marcada le asignamos a todos los productos con esa categoría que están descatalogados
