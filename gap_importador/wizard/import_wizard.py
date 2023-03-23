bbf525ca43806de17abe0a2fe9f25421d7efd240
Juan Manuel Santiago Puentes <juanmasp888@gmail.com>
2023-03-23 11:07:40 +0100

    mensajes de depuracion

--- gap_importador/wizard/import_wizard.py
+++ gap_importador/wizard/import_wizard.py
@@ -118,6 +118,8 @@ class importProductsWizard(models.TransientModel):
                 ).ids
             ids_combinacion = sorted(ids_combinacion)
             ids_combinacion =  ','.join(str(num) for num in ids_combinacion)
+            logger.info("PARA EL TEMPLATE %s IDS COMBINACION: %s" % (template.id,ids_combinacion))
+            logger.info("IDS A AGREGAR: %s" % self.env['product.product'].search([('product_tmpl_id', '=', template.id),('combination_indices', '=', ids_combinacion)]).id)
             ids_no_descatalogados.append(self.env['product.product'].search([('product_tmpl_id', '=', template.id),('combination_indices', '=', ids_combinacion)]).id)
             num_fila += 1        
             logger.info("IDS NO DESCATALOGADOS: %s" % ids_no_descatalogados)
