c948b4de1fb77921ab33dbee2c8c3c6ab26204cf
Juan Manuel Santiago Puentes <juanmasp888@gmail.com>
2023-03-27 12:54:31 +0200

    He métido la función search, modificando como llamo al rpc

--- gap_paragon/static/src/js/escanear_productos.js
+++ gap_paragon/static/src/js/escanear_productos.js
@@ -6,24 +6,24 @@ import BarcodeModel from '@stock_barcode/models/barcode_model';
 patch(BarcodeModel.prototype, 'escanear_productos', {
     
      
-
     async _processBarcode(barcode) {
+
+        var rpc = require('web.rpc');
 
         let barcodeData = {};
         const filters = {};
-        let messaging = new instance.web.Messaging();
 
         console.log("Holaaaa");
         barcodeData = await this._parseBarcode(barcode, filters);
         console.log(barcodeData);
         let codigo = barcodeData.barcode
         
-        const productos = await this.messaging.rpc({
+        const productos = await rpc.query({
             model: 'product.product',
-            method: 'search',
+            method: 'search_read',
             args: [[['id', '=', 5]]],
-            kwargs: {fields: ['name','list_price']}
-        }, { shadow: true });
+            fields: ['name','list_price']
+        });
 
 
         console.log(productos);
