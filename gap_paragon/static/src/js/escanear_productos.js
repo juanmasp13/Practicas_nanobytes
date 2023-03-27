27fdc12c1fd690ab9eb24ff031e678aea890cc19
Juan Manuel Santiago Puentes <juanmasp888@gmail.com>
2023-03-27 11:48:52 +0200

    comprobando herencia

--- gap_paragon/static/src/js/escanear_productos.js
+++ gap_paragon/static/src/js/escanear_productos.js
@@ -4,9 +4,7 @@ import { patch } from 'web.utils';
 import BarcodeModel from '@stock_barcode/models/barcode_model';
 
 patch(BarcodeModel.prototype, 'escanear_productos', {
-    /**
-     * @override
-     */
+    
     async _processBarcode(barcode) {
 
         console.log("Holaaaa");
