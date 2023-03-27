4c7e78c6d1fea0b616423c9dd2cfa6ac78804b77
Juan Manuel Santiago Puentes <juanmasp888@gmail.com>
2023-03-27 12:59:18 +0200

    He cambiado el pallet a Char

--- gap_paragon/models/stock_product_lot_inherit.py
+++ gap_paragon/models/stock_product_lot_inherit.py
@@ -3,7 +3,7 @@ from odoo import fields, models
 class stockProductLotInherit(models.Model):
     _inherit = "stock.production.lot"
 
-    pallet_no = fields.Float(string='Pallet no.', digits=(0, 3))
+    pallet_no = fields.Char(string='Pallet no.')
     pmax = fields.Float(string='Pmax', digits=(0, 3))
     ff = fields.Float(string='FF', digits=(0, 3))
     voc = fields.Float(string='Voc', digits=(0, 3))
