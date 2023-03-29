966492f08c51b23f887cf3ccaedde1f4c92842d3
Juan Manuel Santiago Puentes <juanmasp888@gmail.com>
2023-03-29 14:56:32 +0200

    Creando campo calculado para mostrar el botón o no

--- gap_paragon/models/stock_move_inherit.py
+++ gap_paragon/models/stock_move_inherit.py
@@ -13,3 +13,9 @@ class stockMoveInherit(models.Model):
 class PartnerRiskExceededWizInherit(models.TransientModel):
     _inherit = "partner.risk.exceeded.wiz"
 
+    mostrar_boton = fields.Boolean(compute="_mostrar_boton")
+
+    def _mostrar_boton(self):
+        for record in self:
+            group_ids = self.env['res.users'].browse([self.env.user.id]).groups_id
+            record.mostrar_boton = self._context.get('active_model') == 'sale.order' or group_ids in self.env.ref('account_financial_risk.group_overpass_partner_risk_exception').id
