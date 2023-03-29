9b1b99dc877e1a852be0788a57da379d0bbf6073
Juan Manuel Santiago Puentes <juanmasp888@gmail.com>
2023-03-29 11:07:57 +0200

    Fix

--- gap_paragon/models/stock_move_inherit.py
+++ gap_paragon/models/stock_move_inherit.py
@@ -1,8 +1,11 @@
 from odoo import fields, models
+import logging, io
+logger = logging.getLogger(__name__)
 
 class stockMoveInherit(models.Model):
     _inherit = "stock.move"
 
     fichero = fields.Binary(string="Subir report")
 
-    
+    def registrar_num_serie(self):
+        logger.info("Hola")
