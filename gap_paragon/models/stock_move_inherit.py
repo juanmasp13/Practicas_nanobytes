d22b0f6ce0f036e0b87269c17fffafceaa613ac5
Juan Manuel Santiago Puentes <juanmasp888@gmail.com>
2023-03-30 10:11:47 +0200

    Comprobar que me devuelve el context

--- gap_paragon/models/stock_move_inherit.py
+++ gap_paragon/models/stock_move_inherit.py
@@ -1,5 +1,10 @@
 from odoo import fields, models
+from odoo.tools import DEFAULT_SERVER_DATE_FORMAT, DEFAULT_SERVER_DATETIME_FORMAT
+from odoo.tools.translate import _
 import logging, io
+import base64
+import datetime
+import xlrd
 logger = logging.getLogger(__name__)
 
 class stockMoveInherit(models.Model):
@@ -8,7 +13,63 @@ class stockMoveInherit(models.Model):
     fichero = fields.Binary(string="Subir report")
 
     def registrar_num_serie(self):
-        logger.info("Hola")
+        logger.info("MI CONTEXT: %s" % self._context)
+
+    def leer_excel_sin_cabecera(self):
+        if self.fichero:
+            opt = {}
+            num_filas, filas = self._read_xls(options=opt) #En valores guardo una lista con listas de valores
+            filas.pop(0)
+            #Aquí estan todos los valores excluyendo la cabecera gracias al método pop()
+            return filas
+
+    def _read_xls(self, options):
+        book = xlrd.open_workbook(file_contents=base64.b64decode(self.fichero) or b'') # IMPORTANTE EL base64.b64decode, es necesario hacerselo al campo para poder leerlo
+        sheets = options['sheets'] = book.sheet_names()
+        sheet = options['sheet'] = options.get('sheet') or sheets[0]
+        return self._read_xls_book(book, sheet)
+
+    def _read_xls_book(self, book, sheet_name):
+        sheet = book.sheet_by_name(sheet_name)
+        rows = []
+        # emulate Sheet.get_rows for pre-0.9.4
+        for rowx, row in enumerate(map(sheet.row, range(sheet.nrows)), 1):
+            values = []
+            for colx, cell in enumerate(row, 1):
+                if cell.ctype is xlrd.XL_CELL_NUMBER:
+                    is_float = cell.value % 1 != 0.0
+                    values.append(
+                        str(cell.value)
+                        if is_float
+                        else str(int(cell.value))
+                    )
+                elif cell.ctype is xlrd.XL_CELL_DATE:
+                    is_datetime = cell.value % 1 != 0.0
+                    # emulate xldate_as_datetime for pre-0.9.3
+                    dt = datetime.datetime(*xlrd.xldate.xldate_as_tuple(cell.value, book.datemode))
+                    values.append(
+                        dt.strftime(DEFAULT_SERVER_DATETIME_FORMAT)
+                        if is_datetime
+                        else dt.strftime(DEFAULT_SERVER_DATE_FORMAT)
+                    )
+                elif cell.ctype is xlrd.XL_CELL_BOOLEAN:
+                    values.append(u'True' if cell.value else u'False')
+                elif cell.ctype is xlrd.XL_CELL_ERROR:
+                    raise ValueError(
+                        _("Invalid cell value at row %(row)s, column %(col)s: %(cell_value)s") % {
+                            'row': rowx,
+                            'col': colx,
+                            'cell_value': xlrd.error_text_from_code.get(cell.value, _("unknown error code %s", cell.value))
+                        }
+                    )
+                else:
+                    values.append(cell.value)
+            if any(x for x in values if x.strip()):
+                rows.append(values)
+
+        # return the file length as first value
+        return sheet.nrows, rows
+    
 
 class PartnerRiskExceededWizInherit(models.TransientModel):
     _inherit = "partner.risk.exceeded.wiz"
