23190906cf38e8b82ed0e64fd19222625396d5dd
Juan Manuel Santiago Puentes <juanmasp888@gmail.com>
2023-03-31 14:05:52 +0200

    Comprobar que me devuelve move_line_ids_without_package

---
+++ gap_paragon/models/stock_picking_inherit.py
@@ -0,0 +1,11 @@
+from odoo import fields, models
+from odoo.exceptions import UserError
+
+
+class StockPickingInherit(models.Model):
+    _inherit = "stock.picking"
+
+
+    def button_validate(self):
+        raise UserError(self.move_line_ids_without_package)
+        super(StockPickingInherit, self).button_validate()
