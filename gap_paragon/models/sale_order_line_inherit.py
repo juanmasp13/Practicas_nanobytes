a182c76aab8ced963c857c4f720e7cc39398267b
Juan Manuel Santiago Puentes <juanmasp888@gmail.com>
2023-03-31 12:12:53 +0200

    Haciendo lo mismo que antes pero esta vez en el sale.order

--- gap_paragon/models/sale_order_line_inherit.py
+++ gap_paragon/models/sale_order_line_inherit.py
@@ -1,6 +1,7 @@
 from odoo import fields, models
 
 class SaleOrderLineInherit(models.Model):
-    _inherit = "sale.order.line"
+    _inherit = "sale.order"
 
+    product_id = fields.Many2one('product.product', string="Producto")
     virtual_available = fields.Float(string="Inventario pronosticado", related="product_id.virtual_available")
