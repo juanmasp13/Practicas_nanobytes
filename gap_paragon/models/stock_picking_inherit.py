608c4d3c2ec3f7a128768462fa60b1dbac36ce3e
Juan Manuel Santiago Puentes <juanmasp888@gmail.com>
2023-03-31 14:20:07 +0200

    Comprobar si me hace bien la herencia del método

--- gap_paragon/models/stock_picking_inherit.py
+++ gap_paragon/models/stock_picking_inherit.py
@@ -5,7 +5,8 @@ from odoo.exceptions import UserError
 class StockPickingInherit(models.Model):
     _inherit = "stock.picking"
 
+    #CANTIDAD DISPONIBLE: self.move_line_ids_without_package.product_id.qty_available 
 
     def button_validate(self):        
-        raise UserError(self.move_line_ids_without_package.product_id.qty_available)
+        raise UserError(self.move_line_ids_without_package.qty_available)
         super(StockPickingInherit, self).button_validate()
