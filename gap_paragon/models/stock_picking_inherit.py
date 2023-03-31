591f50829a92f68114bc2b01587ecfb346614118
Juan Manuel Santiago Puentes <juanmasp888@gmail.com>
2023-03-31 14:25:05 +0200

    Comprobación hecha, ahora ver si me hace bien el método

--- gap_paragon/models/stock_picking_inherit.py
+++ gap_paragon/models/stock_picking_inherit.py
@@ -11,4 +11,4 @@ class StockPickingInherit(models.Model):
     def button_validate(self):
         if self.move_line_ids_without_package.qty_done > self.move_line_ids_without_package.product_id.qty_available:
             raise UserError("La cantidad de salida (%s) es mayor que la cantidad de stock disponible (%s)" % (self.move_line_ids_without_package.qty_done,self.move_line_ids_without_package.product_id.qty_available))
-        super(StockPickingInherit, self).button_validate()
+        return super(StockPickingInherit, self).button_validate()
