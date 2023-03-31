fc870eb3da3d3fdfd10c8fe9b3beda62eed7d7b8
Juan Manuel Santiago Puentes <juanmasp888@gmail.com>
2023-03-31 10:29:16 +0200

    Creando un campo relacionado en sale.order.line para poder acceder a él desde la vista xml

---
+++ gap_paragon/models/sale_order_line_inherit.py
@@ -0,0 +1,6 @@
+from odoo import fields, models
+
+class SaleOrderLineInherit(models.Model):
+    _inherit = "sale.order.line"
+
+    virtual_available = fields.Float(string="Inventario pronosticado", related="product_id.virtual_available")
