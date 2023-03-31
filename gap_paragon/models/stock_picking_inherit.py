12989ca89203dfa41e309113bcfea105ea32a53d
Juan Manuel Santiago Puentes <juanmasp888@gmail.com>
2023-03-31 14:56:26 +0200

    He cambiado el campo que utilizo de stock picking, usaba el move_line_ids_without_package y ahora uso el move_line_ids

--- gap_paragon/models/stock_picking_inherit.py
+++ gap_paragon/models/stock_picking_inherit.py
@@ -9,6 +9,6 @@ class StockPickingInherit(models.Model):
     #CANTIDAD EXPORTACION: self.move_line_ids_without_package.qty_done
 
     def button_validate(self):
-        if self.move_line_ids_without_package.qty_done > self.move_line_ids_without_package.product_id.qty_available and self.picking_type_code == 'outgoing':
+        if self.move_line_ids.qty_done > self.move_line_ids.product_id.qty_available and self.picking_type_code == 'outgoing':
             raise UserError("La cantidad de salida (%s) es mayor que la cantidad de stock disponible (%s)" % (self.move_line_ids_without_package.qty_done,self.move_line_ids_without_package.product_id.qty_available))
         return super(StockPickingInherit, self).button_validate()
