a4297535037086d5dcd9c5cfe7b80494589bee6e
Juan Manuel Santiago Puentes <juanmasp888@gmail.com>
2023-04-10 09:18:30 +0200

    Añadiendo el estado al modelo stock.picking

--- gap_paragon/models/stock_picking_inherit.py
+++ gap_paragon/models/stock_picking_inherit.py
@@ -4,7 +4,9 @@ from odoo.exceptions import UserError
 
 class StockPickingInherit(models.Model):
     _inherit = "stock.picking"
-    
+
+    state = fields.Selection(selection_add=[('aprobacion', 'Aprobación'), ('assigned',)])
+
     def button_validate(self):
         if self.move_line_ids.qty_done > self.move_line_ids.product_id.qty_available and self.picking_type_code == 'outgoing':
             raise UserError("La cantidad de salida (%s) es mayor que la cantidad de stock disponible (%s)" % (self.move_line_ids_without_package.qty_done,self.move_line_ids_without_package.product_id.qty_available))
