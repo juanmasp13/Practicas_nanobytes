12e102650ba72dd3d94c202e9b4ddfa3c0343bd2
Juan Manuel Santiago Puentes <juanmasp888@gmail.com>
2023-04-10 10:32:59 +0200

    He vuelto al paso anterior

--- gap_paragon/models/stock_picking_inherit.py
+++ gap_paragon/models/stock_picking_inherit.py
@@ -11,5 +11,7 @@ class StockPickingInherit(models.Model):
         if self.move_line_ids.qty_done > self.move_line_ids.product_id.qty_available and self.picking_type_code == 'outgoing':
             raise UserError("La cantidad de salida (%s) es mayor que la cantidad de stock disponible (%s)" % (self.move_line_ids_without_package.qty_done,self.move_line_ids_without_package.product_id.qty_available))
         
-        
+
         return super(StockPickingInherit, self).button_validate()
+    
+    
