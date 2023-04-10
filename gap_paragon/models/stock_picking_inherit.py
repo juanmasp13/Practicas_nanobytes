4279edfa2e4043951fb4d540efbf561eb6c1d71c
Juan Manuel Santiago Puentes <juanmasp888@gmail.com>
2023-04-10 09:10:58 +0200

    He quitado unos comentarios

--- gap_paragon/models/stock_picking_inherit.py
+++ gap_paragon/models/stock_picking_inherit.py
@@ -4,10 +4,7 @@ from odoo.exceptions import UserError
 
 class StockPickingInherit(models.Model):
     _inherit = "stock.picking"
-
-    #CANTIDAD DISPONIBLE: self.move_line_ids_without_package.product_id.qty_available 
-    #CANTIDAD EXPORTACION: self.move_line_ids_without_package.qty_done
-
+    
     def button_validate(self):
         if self.move_line_ids.qty_done > self.move_line_ids.product_id.qty_available and self.picking_type_code == 'outgoing':
             raise UserError("La cantidad de salida (%s) es mayor que la cantidad de stock disponible (%s)" % (self.move_line_ids_without_package.qty_done,self.move_line_ids_without_package.product_id.qty_available))
