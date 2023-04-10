8fedd22518055fa6e01c484f5f890aa28d0e9377
Juan Manuel Santiago Puentes <juanmasp888@gmail.com>
2023-04-10 11:16:16 +0200

    En vez de hacer un selection add, reescribo el selection entero

--- gap_paragon/models/stock_picking_inherit.py
+++ gap_paragon/models/stock_picking_inherit.py
@@ -6,14 +6,38 @@ class StockPickingInherit(models.Model):
     _inherit = "stock.picking"
     
     def _compute_selection(self):
-        selection = [('',),]
         if self.picking_type_code == 'outgoing':
-            selection = [('aprobacion', 'Aprobación'), ('assigned',)]
+            selection = [
+            ('draft', 'Draft'),
+            ('waiting', 'Waiting Another Operation'),
+            ('confirmed', 'Waiting'),
+            ('assigned', 'Ready'),
+            ('aprobacion', 'Aprobación'),
+            ('done', 'Done'),
+            ('cancel', 'Cancelled'),
+        ]
+        else:
+            selection = [
+            ('draft', 'Draft'),
+            ('waiting', 'Waiting Another Operation'),
+            ('confirmed', 'Waiting'),
+            ('assigned', 'Ready'),
+            ('done', 'Done'),
+            ('cancel', 'Cancelled'),
+        ]
         
         return selection
     
 
-    state = fields.Selection(selection_add='_compute_selection(self)') 
+    state = fields.Selection(selection='_compute_selection', string='Status', compute='_compute_state',
+        copy=False, index=True, readonly=True, store=True, tracking=True,
+        help=" * Draft: The transfer is not confirmed yet. Reservation doesn't apply.\n"
+             " * Waiting another operation: This transfer is waiting for another operation before being ready.\n"
+             " * Waiting: The transfer is waiting for the availability of some products.\n(a) The shipping policy is \"As soon as possible\": no product could be reserved.\n(b) The shipping policy is \"When all products are ready\": not all the products could be reserved.\n"
+             " * Ready: The transfer is ready to be processed.\n(a) The shipping policy is \"As soon as possible\": at least one product has been reserved.\n(b) The shipping policy is \"When all products are ready\": all product have been reserved.\n"
+             " * Done: The transfer has been processed.\n"
+             " * Aprobación: Un administrador debe aprobar validar el albarán.\n"
+             " * Cancelled: The transfer has been cancelled.") 
 
     def button_validate(self):
         if self.move_line_ids.qty_done > self.move_line_ids.product_id.qty_available and self.picking_type_code == 'outgoing':
