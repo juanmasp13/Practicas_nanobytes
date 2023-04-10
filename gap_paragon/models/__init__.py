aee4510ee50d502b8a8c533d52dcadaadcdde3bf
Juan Manuel Santiago Puentes <juanmasp888@gmail.com>
2023-04-10 13:30:50 +0200

    he añadido el archivo py al init

--- gap_paragon/models/__init__.py
+++ gap_paragon/models/__init__.py
@@ -2,3 +2,4 @@ from . import stock_product_lot_inherit
 from . import stock_move_inherit
 from . import sale_order_line_inherit
 from . import stock_picking_inherit
+from . import partner_risk_exceeded_wiz_inherit
