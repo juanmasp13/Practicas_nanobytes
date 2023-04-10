965871dafe9ff12bc84f97c28b9d1ed9379bdf38
Juan Manuel Santiago Puentes <juanmasp888@gmail.com>
2023-04-10 12:53:10 +0200

    En vez de hacer un selection add, reescribo el selection entero

--- gap_paragon/models/stock_picking_inherit.py
+++ gap_paragon/models/stock_picking_inherit.py
@@ -12,8 +12,8 @@ class StockPickingInherit(models.Model):
         # logger.info("SOY %s " % self)
         params = self._context.get('params')
         if params:
-            logger.info("Hay parametros")
-            logger.info(params)
+            id_picking = self.env['stock.picking'].browse(params[id])
+            logger.info("id picking es %s" % id_picking)
         # logger.info(self.env.context.get('params')['id'])
         # id = self.env.context.get('params')['id']
         # registro = self.env['stock.picking'].browse(id).picking_type_code
