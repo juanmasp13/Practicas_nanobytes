9b7d0dbbd2580482b705994ea0c1511a89247c61
Juan Manuel Santiago Puentes <juanmasp888@gmail.com>
2023-04-11 14:47:57 +0200

    Vista gantt hecha correctamente

--- gap_informes/report/hr_leave_report_calendar_inherit.py
+++ gap_informes/report/hr_leave_report_calendar_inherit.py
@@ -80,7 +80,7 @@ class LeaveReportCalendar(models.Model):
     def _read(self, fields):
         res = super()._read(fields)
         if self.env.context.get('hide_employee_name') and 'employee_id' in self.env.context.get('group_by', []):
-            name_field = self._fields['name_type']
+            name_field = self._fields['name']
             for record in self.with_user(SUPERUSER_ID):
                 self.env.cache.set(record, name_field, list(record.name.values())[0])
         return res
