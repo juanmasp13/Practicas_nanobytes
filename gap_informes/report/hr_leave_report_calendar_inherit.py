a974c253f4ddf47e7dd0213db3c872dd1467242b
Juan Manuel Santiago Puentes <juanmasp888@gmail.com>
2023-04-11 12:57:49 +0200

    He vuelto a cambiar los AS del SQL para que me modifique solo la vista gantt y no la calendar

--- gap_informes/report/hr_leave_report_calendar_inherit.py
+++ gap_informes/report/hr_leave_report_calendar_inherit.py
@@ -81,7 +81,7 @@ class LeaveReportCalendar(models.Model):
         if self.env.context.get('hide_employee_name') and 'employee_id' in self.env.context.get('group_by', []):
             name_field = self._fields['name_type']
             for record in self.with_user(SUPERUSER_ID):
-                self.env.cache.set(record, name_field, list(record.name_type.values())[0])
+                self.env.cache.set(record, name_field, record.name_type)
         return res
 
     @api.model
