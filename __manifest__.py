e1c07befffefd24e49f5a7d0fa2e7b431ce17ec0
Juan Manuel Santiago Puentes <juanmasp888@gmail.com>
2023-04-11 09:46:41 +0200

    agregando init y manifest

---
+++ __manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'GAP V1 Informe todas las asuencias',
+    'sequence': -1006,
+    'summary': 'Informe sobre todas las ausencias',
+    'description': """Informe sobre todas las ausencias""",
+    'depends': ['hr_holidays'],
+    'data': [],
+    'auto_install': False,
+    'application': True
+}
