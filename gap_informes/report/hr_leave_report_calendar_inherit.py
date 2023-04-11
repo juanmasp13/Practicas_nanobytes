5035d6b54277f3d3bbc24dee0d672a4980bafc1a
Juan Manuel Santiago Puentes <juanmasp888@gmail.com>
2023-04-11 12:41:27 +0200

    He sobreescrito completamente la clase, puesto que con la herencia no me modificaba el método _read, y ahora lo he modificado haciendo que recoja el diccionario del tipo de ausencia y ponga el valor

--- gap_informes/report/hr_leave_report_calendar_inherit.py
+++ gap_informes/report/hr_leave_report_calendar_inherit.py
@@ -80,7 +80,7 @@ class LeaveReportCalendar(models.Model):
         if self.env.context.get('hide_employee_name') and 'employee_id' in self.env.context.get('group_by', []):
             name_field = self._fields['name']
             for record in self.with_user(SUPERUSER_ID):
-                self.env.cache.set(record, name_field, record.name.values())
+                self.env.cache.set(record, name_field, list(record.name.values())[0])
         return res
 
     @api.model
