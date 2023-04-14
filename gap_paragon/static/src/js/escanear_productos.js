4ba4caf5de8754bdb5e2a520d98bcbc76c9ef869
Juan Manuel Santiago Puentes <juanmasp888@gmail.com>
2023-04-14 10:51:15 +0200

    arreglando método de escanear productos

--- gap_paragon/static/src/js/escanear_productos.js
+++ gap_paragon/static/src/js/escanear_productos.js
@@ -42,8 +42,9 @@ patch(BarcodeModel.prototype, 'escanear_productos', {
                     method: 'create',
                     args: [move_lines_to_create],
                 }).then(function(result){
-                    console.log("LOS MOVE LINES CREADOS")
-                    console.log(result)
+                    console.log('result:', result);
+                }).catch(function(error) {
+                    console.log('Error al crear los registros:', error);
                 });
             }
         }
