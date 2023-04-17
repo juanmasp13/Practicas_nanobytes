c34be12da661f4904f75bc898917f6d26befa0d8
Juan Manuel Santiago Puentes <juanmasp888@gmail.com>
2023-04-17 13:54:33 +0200

    Creando init y manifest

---
+++ gap_aprim/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'GAP 4 APRIM FABRICACIÓN PARCIAL',
+    'sequence': -1007,
+    'summary': 'Ordenes de producción parciales',
+    'description': """Ordenes de producción parciales""",
+    'depends': ['base','mrp','mrp_workorder','quality_control'],
+    'data': [],
+    'auto_install': False,
+    'application': True
+}
