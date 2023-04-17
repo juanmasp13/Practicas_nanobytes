35bf71bf7914ad3cfc3c7e16ad1204556eaea373
Juan Manuel Santiago Puentes <juanmasp888@gmail.com>
2023-04-17 09:52:51 +0200

    Creando init y manifest

---
+++ gap_pantoja/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'GAP V18 PANTOJA OCULTAR CAMPOS PERFIL',
+    'sequence': -1006,
+    'summary': 'Ocultar campos perfil',
+    'description': """Ocultar campos perfil""",
+    'depends': ['base'],
+    'data': [],
+    'auto_install': False,
+    'application': True
+}
