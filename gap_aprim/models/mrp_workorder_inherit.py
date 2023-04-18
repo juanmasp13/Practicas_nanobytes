fda86d56a2b4072607d7c23bded5bd1092a75ce5
Juan Manuel Santiago Puentes <juanmasp888@gmail.com>
2023-04-18 14:14:10 +0200

    Agregando la opción de retirar del stock la cantidad de producto de las ordenes de trabajo

--- gap_aprim/models/mrp_workorder_inherit.py
+++ gap_aprim/models/mrp_workorder_inherit.py
@@ -19,5 +19,6 @@ class MrpProductionWorkcenterLine(models.Model):
             logger.info(qty_result)
             logger.info(id.move_id)
             logger.info(id.move_line_id.qty_done)
+            logger.info(id.move_line_id.product_qty)
         #return super(MrpProductionWorkcenterLine, self).do_finish()
             
