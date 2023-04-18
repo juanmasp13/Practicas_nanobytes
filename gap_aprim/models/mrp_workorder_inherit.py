213ff3e703323dc5cd5dea5c3bb783a71385535c
Juan Manuel Santiago Puentes <juanmasp888@gmail.com>
2023-04-18 13:21:02 +0200

    Agregando la opción de retirar del stock la cantidad de producto de las ordenes de trabajo

--- gap_aprim/models/mrp_workorder_inherit.py
+++ gap_aprim/models/mrp_workorder_inherit.py
@@ -11,8 +11,16 @@ class MrpProductionWorkcenterLine(models.Model):
     def do_finish(self):
         for id in self.check_ids:
             qty_result = id.component_id.qty_available - id.qty_done
+            logger.info("CANTIDAD ANTERIOR")
+            logger.info(id.component_id.qty_available)
+            logger.info("CANTIDAD A RESTAR")
+            logger.info(id.qty_done)
             new_qty = id.component_id.write({'qty_available': qty_result})
+            logger.info("NUEVA CANTIDAD")
+            logger.info(qty_result)
+            logger.info("EL WRITE")
+            logger.info(new_qty)
             if not new_qty:
                 raise UserError("No se ha podido actualizar la cantidad de %s" % id.component_id.name)
-        return super(MrpProductionWorkcenterLine, self).do_finish()
+        #return super(MrpProductionWorkcenterLine, self).do_finish()
             
