ded650d0a00ca72c3c4e6e37a14120592559809b
Juan Manuel Santiago Puentes <juanmasp888@gmail.com>
2023-04-19 11:32:49 +0200

    Depurando método button_mark_done

--- gap_aprim/models/mrp_workorder_inherit.py
+++ gap_aprim/models/mrp_workorder_inherit.py
@@ -1,7 +1,6 @@
 from odoo import api, fields, models, _
 from odoo.osv import expression
 from odoo.exceptions import UserError, ValidationError
-from odoo.tools import float_compare, float_round, float_is_zero
 import logging
 logger = logging.getLogger(__name__)
 
