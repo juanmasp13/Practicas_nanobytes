13d2a327735fae9ec6ca7a2c259925bb071e9ce7
Juan Manuel Santiago Puentes <juanmasp888@gmail.com>
2023-04-19 09:43:49 +0200

    Heredando el método button_finish para retirar el stock de los componentes

--- gap_aprim/models/mrp_workorder_inherit.py
+++ gap_aprim/models/mrp_workorder_inherit.py
@@ -9,30 +9,5 @@ class MrpProductionWorkcenterLine(models.Model):
     _inherit = "mrp.workorder"
 
     def do_finish(self):
-        for id in self.check_ids:
-            qty_result = id.component_id.qty_available - id.qty_done
-            logger.info("CANTIDAD ANTERIOR")
-            logger.info(id.component_id.qty_available)
-            logger.info("CANTIDAD A RESTAR")
-            logger.info(id.qty_done)
-            logger.info("NUEVA CANTIDAD")
-            logger.info(qty_result)
-            logger.info(id.move_id)
-            logger.info("---------------")
-            logger.info("product_uom_qty")
-            logger.info(id.move_id.product_uom_qty)
-            logger.info("product_qty_available")
-            logger.info(id.move_id.product_uom_qty)
-            logger.info("quantity_done")
-            logger.info(id.move_id.quantity_done)
-            logger.info("product_qty")
-            logger.info(id.move_id.product_qty)
-            logger.info("should_consume_qty")
-            logger.info(id.move_id.should_consume_qty)
-            logger.info("reserved_availability")
-            logger.info(id.move_id.reserved_availability)
-            logger.info("product_virtual_available")
-            logger.info(id.move_id.product_virtual_available)
-            logger.info("---------------")
-        #return super(MrpProductionWorkcenterLine, self).do_finish()
+        return super(MrpProductionWorkcenterLine, self).button_finish()
             
