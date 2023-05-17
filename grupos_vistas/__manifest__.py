3757ed311d406f8368c05775b4cae2edd6d7b5ce
Juan Manuel Santiago Puentes <juanmasp888@gmail.com>
2023-05-17 12:27:18 +0200

    vista

---
+++ grupos_vistas/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name' : 'Grupos vistas',
+    'version' : '1.2',
+    'summary': 'Invoices & Payments',
+    'sequence': 10,
+    'description': """
+        grupos
+    """,
+    'category': 'Accounting/Accounting',
+    'depends' : ['base_setup'],
+    'data': [],
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
+}
