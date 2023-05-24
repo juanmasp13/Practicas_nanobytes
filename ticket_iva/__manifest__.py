ab959750310fe3ee01e7ea3240680e1edd791c49
Juan Manuel Santiago Puentes <juanmasp888@gmail.com>
2023-05-24 10:02:24 +0200

    [update] init y manifest

---
+++ ticket_iva/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'TICKET IVA',
+    'sequence': -1005,
+    'summary': 'Problemas con iva en módulo l10n_es_vat_book',
+    'description': """Problemas con iva en módulo l10n_es_vat_book""",
+    'depends': ['l10n_es_vat_book'],
+    'data': [],
+    'auto_install': False,
+    'application': True
+}
